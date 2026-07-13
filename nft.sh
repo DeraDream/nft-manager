db487eb4f1696930210de9f915dcbea946c33687
DeraDream <474530741@qq.com>
2026-07-13 14:54:05 +0800

    Fix web auth fallback

--- nft.sh
+++ nft.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #
-# nftables 端口转发管理工具 v1.2
+# nftables 端口转发管理工具 v1.3
 # 交互式管理 DNAT 端口转发规则
 #
 
 # ============== 常量定义 ==============
-SCRIPT_VERSION="1.2"
+SCRIPT_VERSION="1.3"
 CONF_DIR="/etc/nftables.d"
 CONF_FILE="${CONF_DIR}/port-forward.conf"
 TARGETS_FILE="${CONF_DIR}/targets.conf"
