af3488f5cb6328e2e6c4e3e3579678f545de547a
DeraDream <474530741@qq.com>
2026-07-15 18:22:41 +0800

    Improve chart interaction and list sorting

--- nft.sh
+++ nft.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #
-# nftables 端口转发管理工具 v3.22
+# nftables 端口转发管理工具 v3.23
 # 交互式管理 DNAT 端口转发规则
 #
 
 # ============== 常量定义 ==============
-SCRIPT_VERSION="3.22"
-WEB_PANEL_VERSION="3.22"
+SCRIPT_VERSION="3.23"
+WEB_PANEL_VERSION="3.23"
 CONF_DIR="/etc/nftables.d"
 CONF_FILE="${CONF_DIR}/port-forward.conf"
 TARGETS_FILE="${CONF_DIR}/targets.conf"
