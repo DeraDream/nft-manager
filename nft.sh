49983feb9dd6faf59d736e92ab181cf68f77b044
DeraDream <474530741@qq.com>
2026-07-15 14:41:34 +0800

    Add responsive mobile web interface

--- nft.sh
+++ nft.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #
-# nftables 端口转发管理工具 v3.9
+# nftables 端口转发管理工具 v3.10
 # 交互式管理 DNAT 端口转发规则
 #
 
 # ============== 常量定义 ==============
-SCRIPT_VERSION="3.9"
-WEB_PANEL_VERSION="3.9"
+SCRIPT_VERSION="3.10"
+WEB_PANEL_VERSION="3.10"
 CONF_DIR="/etc/nftables.d"
 CONF_FILE="${CONF_DIR}/port-forward.conf"
 TARGETS_FILE="${CONF_DIR}/targets.conf"
