3dbc5b303f129c3c1de81a955f6e1893ab8ef031
DeraDream <474530741@qq.com>
2026-07-15 16:54:09 +0800

    Document temporary offline extraction

--- nft.sh
+++ nft.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #
-# nftables 端口转发管理工具 v3.17
+# nftables 端口转发管理工具 v3.18
 # 交互式管理 DNAT 端口转发规则
 #
 
 # ============== 常量定义 ==============
-SCRIPT_VERSION="3.17"
-WEB_PANEL_VERSION="3.17"
+SCRIPT_VERSION="3.18"
+WEB_PANEL_VERSION="3.18"
 CONF_DIR="/etc/nftables.d"
 CONF_FILE="${CONF_DIR}/port-forward.conf"
 TARGETS_FILE="${CONF_DIR}/targets.conf"
